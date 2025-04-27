85127e9a49aaf4ac16364acdafdf8daff9b4d78d
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2025-04-27 16:48:01 +0300

    v 0.21.1

--- menu.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
-
-
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://level.tscn")
---
+++ scenes/menu.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
